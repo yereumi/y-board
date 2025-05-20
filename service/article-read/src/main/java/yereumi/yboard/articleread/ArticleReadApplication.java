14a0967b5ab5d39fcdd283f840b10be6f1f65910
yereumi <lovelyyeji01@gmail.com>
2025-05-20 13:46:16 +0900

    chore: 모듈 초기 설정

---
+++ service/article-read/src/main/java/yereumi/yboard/articleread/ArticleReadApplication.java
@@ -0,0 +1,12 @@
+package yereumi.yboard.articleread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleReadApplication.class, args);
+    }
+}
---
+++ service/view/src/main/java/yereumi/yboard/view/ViewApplication.java
@@ -0,0 +1,12 @@
+package yereumi.yboard.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewApplication.class, args);
+    }
+}
